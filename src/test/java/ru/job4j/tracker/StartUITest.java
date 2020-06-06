89ccd79feba1877791146446aae42a9e3ca9f8fc
Foneom <mugoloka@gmail.com>
2020-06-06 20:03:46 +0300

    9.2. Тесты вывода на консоль в StartUI[33585#281563]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,21 +7,43 @@ import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+
+
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
+    public void whenFindItemById() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-       // StartUI.(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
+        Item item = tracker.add(new Item("Item"));
+        Input in = new StubInput(
+                new String[] {"0", item.getId(), "1"}
+        );
+        UserAction[] actions = {
+                new FindItemById(),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()), is(item));
+    }
+    @Test
+    public void whenFindItemByName() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Item"));
+        Input in = new StubInput(
+                new String[] {"0", item.getName(), "1"}
+        );
+        UserAction[] actions = {
+                new FindItemByName(),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findByName(item.getName())[0], is(item));
     }
         @Test
         public void whenReplaceItem() {
+            Output out = new StubOutput();
             Tracker tracker = new Tracker();
             Item item = tracker.add(new Item("Replaced item"));
-            /* Входные данные должны содержать ID добавленной заявки item.getId() */
             String replacedName = "New item name";
             Input in = new StubInput(
                     new String[] {"0", item.getId(), "1"}
@@ -30,11 +52,12 @@ public class StartUITest {
                     new ReplaceAction(),
                     new ExitAction()
             };
-            new StartUI().init(in, tracker, actions);
+            new StartUI(out).init(in, tracker, actions);
             assertThat(tracker.findById(item.getId()), is(replacedName));
         }
 @Test
 public void whenDeleteItem() {
+    Output out = new StubOutput();
     Tracker tracker = new Tracker();
     Item item = tracker.add(new Item("Deleted item"));
     Input in = new StubInput(
@@ -44,21 +67,37 @@ public void whenDeleteItem() {
             new DeleteAction(),
             new ExitAction()
     };
-    new StartUI().init(in, tracker, actions);
+    new StartUI(out).init(in, tracker, actions);
     assertThat(tracker.findById(item.getId()), is(nullValue()));
 }
-
     @Test
     public void whenCreateItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
+                new CreateAction(out),
                 new ExitAction()
         };
-        new StartUI().init(in, tracker, actions);
+       new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+    @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
     }
 }
