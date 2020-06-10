38381a185a3d6f471c728545705e93b9f86ea0ef
Foneom <mugoloka@gmail.com>
2020-06-10 13:15:33 +0300

    1.1. Тесты на ValidateInput.[223313#281588]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
+
 public class StartUITest {
 
 
@@ -98,6 +99,27 @@ public void whenDeleteItem() {
         assertThat(out.toString(), is(
                 "Menu. " + System.lineSeparator() +
                         "0. Exit" + System.lineSeparator()
+        ));
+    }
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu. %n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0... 0%n"
+                                + "Menu. %n"
+                                + "0. Exit%n"
+                )
         ));
     }
 }
