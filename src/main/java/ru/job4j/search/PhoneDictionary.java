c1b06dc5aed115177768f0e5f654c96a505deebd
Foneom <mugoloka@gmail.com>
2020-06-17 23:50:11 +0300

    1. Телефонный справочник на базе ArrayList[41598#281543]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            result.add(person);
+            if (
+                            person.getName().contains(key) ||
+                            person.getSurname().contains(key) ||
+                            person.getAddress().contains(key) ||
+                            person.getPhone().contains(key)
+            )
+            return result;
+        }
+        return null;
+    }
+}
+
