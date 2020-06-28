0cefd9e1d25d33c309ce5443a3b12f3c1c1b084a
Foneom <mugoloka@gmail.com>
2020-06-28 20:52:19 +0300

    3. Компаратор для строк[35008#281537] исправление

--- src/main/java/ru/job4j/block_3collections/sort/StringCompare.java
+++ src/main/java/ru/job4j/block_3collections/sort/StringCompare.java
@@ -5,22 +5,10 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        char[] left1 = left.toCharArray();
-        char[] right1 = right.toCharArray();
-        int[] left2 = new int[left1.length];
-        int[] right2 = new int[right1.length];
-        int result = 0;
-        for (int i = 0; i < Math.min(left1.length, left2.length); i++) {
-            left2[i] = Character.getNumericValue(left1[i]);
-            right2[i] = Character.getNumericValue(right1[i]);
-            if ((left2[i] < right2[i] && left2.length != right2.length)
-                    || (left2[i] < right2[i] && left2.length == right2.length)) {
-                result = -1;
-                break;
-            } else if ((left2[i] > right2[i] && left2.length != right2.length)
-                    || (left2[i] > right2[i] && left2.length == right2.length)) {
-                result = 1;
-                break;
+        int result = left.length() - right.length();
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
             return result;
