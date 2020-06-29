42bb5a3a515e0ab9842a32ad14cf4c00db579861
Foneom <mugoloka@gmail.com>
2020-06-29 19:03:24 +0300

    4. Сортировка номера[221284#281538] исправление

--- src/main/java/ru/job4j/block3collections/sort/LexSort.java
+++ src/main/java/ru/job4j/block3collections/sort/LexSort.java
@@ -6,24 +6,10 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int diff = left.length() - right.length();
-        /**
-         * Делим строки
-         */
         String[] leftLine = left.split(". ");
         String[] rightLine = right.split(". ");
-        /**
-         * Преобразуем строковую цифру в примитивный тип
-         */
         int leftLineInt = Integer.parseInt(leftLine[0]);
         int rightLineInt = Integer.parseInt(rightLine[0]);
-        /**
-         * Сравниваем значения, если не равны, то идет сортировка по убыванию,
-         * если равны, то большей считается длинная сторка
-         */
-        if (leftLineInt != rightLineInt) {
-           return Integer.compare(leftLineInt, rightLineInt);
-        }
-        return diff;
+        return Integer.compare(leftLineInt, rightLineInt);
     }
 }
