a663bd2f2d12e5dbea9f90ad898f16d381c92cc5
Foneom <mugoloka@gmail.com>
2020-07-07 17:05:06 +0300

    1. Фильтрация учеников.[110058#281407] исправление

--- src/main/java/ru/job4j/block4lambda/lambda/Func.java
+++ src/main/java/ru/job4j/block4lambda/lambda/Func.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class Func {
-        public static List<Double> diapason ( int start, int end, Function<Double, Double> func) {
+        public static List<Double> diapason (int start, int end, Function<Double, Double> func) {
             List<Double> rsl = new ArrayList<>();
             for (int i = start; i < end; i++) {
                 double result = func.apply((double) i);
--- src/main/java/ru/job4j/block4lambda/stream/student/School.java
+++ src/main/java/ru/job4j/block4lambda/stream/student/School.java
@@ -9,27 +9,27 @@ public class School {
     public static List<Student> collect(List<Student> students,
                                         Predicate<Student> predicate) {
         List<Student> rsl = students.stream()
-                .filter(student -> predicate.test(student))
+                .filter(predicate)
                 .collect(Collectors.toList());
         return rsl;
     }
 
     public static List<Student> classA(List<Student> students) {
         Predicate<Student> getStudentsClassA = student ->
-                student.getScore() > 70 && student.getScore() < 100;
+                student.getScore() >= 70 && student.getScore() <= 100;
         return collect(students, getStudentsClassA);
     }
 
     public static List<Student> classB(List<Student> students) {
         Predicate<Student> getStudentsClassB = student ->
-                student.getScore() > 50 && student.getScore() < 70;
+                student.getScore() >= 50 && student.getScore() <= 70;
         return collect(students, getStudentsClassB);
     }
 
     public static List<Student> classV(List<Student> students) {
-        Predicate<Student> getStudentsClassC = student ->
-                student.getScore() > 0 && student.getScore() < 50;
-        return collect(students, getStudentsClassC);
+        Predicate<Student> getStudentsClassV = student ->
+                student.getScore() >= 0 && student.getScore() <= 50;
+        return collect(students, getStudentsClassV);
     }
 
 }
