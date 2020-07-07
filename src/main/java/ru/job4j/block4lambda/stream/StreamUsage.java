7742ff5862aef0cd3c5261d0f869bc459006a7ec
Foneom <mugoloka@gmail.com>
2020-07-07 10:49:46 +0300

    0.1. Отладка лямбда в IDEA[259165#281413]

--- src/main/java/ru/job4j/block4lambda/stream/StreamUsage.java
+++ src/main/java/ru/job4j/block4lambda/stream/StreamUsage.java
@@ -1,9 +1,6 @@
 package ru.job4j.block4lambda.stream;
 
 import java.util.List;
-import java.util.Collections;
-import java.util.stream.Collectors;
-
 
 public class StreamUsage {
     public static class Task {
@@ -14,24 +11,19 @@ public class StreamUsage {
             this.name = name;
             this.spent = spent;
         }
+    }
 
-        @Override
-        public String toString() {
-            return "Task{" +
-                    "name='" + name + '\'' +
-                    ", spent=" + spent +
-                    '}';
-        }
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 10),
+                new Task("Task #2", 20),
+                new Task("Bug #3", 40)
+        );
+        tasks.stream()
+                .filter(task -> task.name.contains("Bug"))
+                .filter(task -> task.spent > 30)
+                .map(task -> task.name + " " + task.spent)
+                .forEach(System.out::println);
     }
-        public static void main(String[] args) {
-            List<Task> tasks = List.of(
-                    new Task("Bug #1", 100),
-                    new Task("Task #2", 100),
-                    new Task("Bug #3", 100)
-            );
-            List<Task> bugs = tasks.stream().filter(
-                    task -> task.name.contains("Bug")
-            ).collect(Collectors.toList());
-            bugs.forEach(System.out::println);
-        }
-    }
+}
+
