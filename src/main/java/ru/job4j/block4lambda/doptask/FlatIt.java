92ca5eb8a63bc4e9e50fd76e0e52b8cad4704111
Foneom <mugoloka@gmail.com>
2020-07-20 22:57:15 +0300

    доп задача stream

--- src/main/java/ru/job4j/block4lambda/doptask/FlatIt.java
+++ src/main/java/ru/job4j/block4lambda/doptask/FlatIt.java
@@ -1,21 +1,23 @@
 package ru.job4j.block4lambda.doptask;
 
+import ru.job4j.block3collections.dopTask.Parentheses;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Даны вложенные итераторы. Нужно написать метод, который соберет все элементы их итераторов в List.
  */
 public class FlatIt {
     public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
-        List<Integer> list = new ArrayList<>();
-        Iterator<Integer> itr = list.iterator();
-        while (it.hasNext() && !itr.hasNext()) {
-            itr = it.next();
-            list.add(itr.next());
-        }
-        list.add(itr.next());
-        return list;
+        Iterable<Integer> iterable = it::next;
+            return StreamSupport
+                    .stream(iterable.spliterator(), false)
+                    .filter(integer -> it.hasNext())
+                    .collect(Collectors.toList());
     }
 }
